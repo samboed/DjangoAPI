f0ca1b1527e2830267a45b2bbf69d51d70a392b6
Eduard Azimov <eduard.azimoff@yandex.ru>
2026-08-23 18:00:14 +0300

    Add testing view

--- logistic/views.py
+++ logistic/views.py
@@ -2,6 +2,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -31,3 +33,8 @@ class StockViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['products']
     pagination_class = StockPagination
+
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response({'message': 'Successful deploy!'})
